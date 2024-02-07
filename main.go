a354787713078d040be9c7d088f1f78a91405e9f
Ago Lepiksaar <alepiksaar@gmail.com>
2024-02-08 01:15:17 +0200

    finished first filter fersion and started compare function.

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"cars/searchbars"
 	"cars/structs"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -11,10 +12,12 @@ import (
 
 var temp *template.Template
 var Car2 = []structs.Models{}
+var Search = structs.SbarVal2{}
+var CompList = []structs.Models{}
 
 func main() {
 
-	address := ":8088"
+	address := ":8081"
 	// making one static folder for staic files example: css
 	fileHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
 	http.Handle("/static/", fileHandler)
@@ -28,6 +31,7 @@ func main() {
 
 	http.HandleFunc("/", HandleFunc)
 	http.HandleFunc("/action", Action)
+	http.HandleFunc("/compare", Compare)
 	http.ListenAndServe(address, nil)
 
 }
@@ -46,8 +50,8 @@ func HandleFunc(w http.ResponseWriter, r *http.Request) {
 	temp.Execute(w, manu)
 }
 
-func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve data from server and send to it
-	log.Println("POST Input Data...")
+func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve data from server and send updated list back.
+	log.Println("POST loading search Data...")
 	var cyearInt int
 	var hpInt int
 	var err error
@@ -74,7 +78,7 @@ func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve da
 		}
 	}
 	// we load te values we recieved from the sidenavbar
-	search := structs.SbarVal2{
+	Search = structs.SbarVal2{
 		ManuN:   r.FormValue("manufacturer"),
 		ManuC:   r.FormValue("country"),
 		Cat:     r.FormValue("category"),
@@ -85,7 +89,7 @@ func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve da
 		Hp:      hpInt,
 		Trans:   r.FormValue("transmission"),
 	}
-	Car2 := searchbars.FilterSearch(search, Car2)
+	Car2 := searchbars.FilterSearch(Search, Car2)
 	searchbar := searchbars.FindSearch()
 
 	manu := map[string]interface{}{
@@ -94,4 +98,57 @@ func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve da
 	}
 	temp.Execute(w, manu)
 
+}
+
+func Compare(w http.ResponseWriter, r *http.Request) {
+	// choices := []structs.SbarVal2{}
+	var cyearInt int
+	var hpInt int
+	var err error
+
+	// FormVale gives us a "string" so we need to convert it into a int
+	yearStr := r.FormValue("carYear")
+	if yearStr == "" {
+		cyearInt = 0
+	} else {
+		cyearInt, err = strconv.Atoi(yearStr)
+		if err != nil {
+			http.Error(w, "Invalid cyear parameter", http.StatusBadRequest)
+			return
+		}
+	}
+	hpStr := r.FormValue("carHp")
+	if hpStr == "" {
+		hpInt = 0
+	} else {
+		hpInt, err = strconv.Atoi(hpStr)
+		if err != nil {
+			http.Error(w, "Invalid Hp parameter", http.StatusBadRequest)
+			return
+		}
+	}
+	// we load te values we recieved from the sidenavbar
+	comp := structs.Models{
+		Name:  r.FormValue("carName"),
+		Year:  cyearInt,
+		Image: r.FormValue("carimage"),
+		Specifications: structs.Specifications{
+			Engine:       r.FormValue("carEngine"),
+			Horsepower:   hpInt,
+			Transmission: r.FormValue("carTransmission"),
+			Drivetrain:   r.FormValue("carDrivetrain"),
+		},
+	}
+	CompList = append(CompList, comp)
+
+	fmt.Println(comp)
+	fmt.Println(CompList)
+
+	searchbar := searchbars.FindSearch()
+
+	manu := map[string]interface{}{
+		"search": searchbar,
+		"car":    Car2,
+	}
+	temp.Execute(w, manu)
 }
