51bc247213015475f12e58e7772c410c08af99cd
Ago Lepiksaar <alepiksaar@gmail.com>
2024-02-19 15:24:59 +0200

    added last required functions. moved frontend to frontend folder.

--- main.go
+++ main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"cars/searchbars"
 	"cars/structs"
-	"fmt"
+	"context"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"strconv"
+	"syscall"
+	"time"
 )
 
 var temp *template.Template
@@ -18,44 +22,77 @@ var CompList = []structs.Models{}
 func main() {
 
 	address := ":8081"
+
+	// declaring multiplex for server
+	mux := http.NewServeMux()
 	// making one static folder for staic files example: css
-	fileHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("static")))
-	http.Handle("/static/", fileHandler)
+	fileHandler := http.StripPrefix("/frontend/", http.FileServer(http.Dir("frontend")))
+	mux.Handle("/frontend/", fileHandler)
 	// declaring all folders under api as static to load the picture
 	fileHandler = http.StripPrefix("/api/", http.FileServer(http.Dir("api")))
-	http.Handle("/api/", fileHandler)
+	mux.Handle("/api/", fileHandler)
 	// declaring car2 here because i can change this struct later.
 	Car2 = searchbars.ModelsElement()
 
 	log.Printf("Starting server on %s", address)
 
-	http.HandleFunc("/", HandleFunc)
-	http.HandleFunc("/action", Action)
-	http.HandleFunc("/compare", Compare)
-	http.HandleFunc("/comparepage", Comparepage)
-	http.HandleFunc("/manufacturer", ManufactPage)
-	http.ListenAndServe(address, nil)
+	mux.HandleFunc("/", HandleFunc)
+	mux.HandleFunc("/action", Action)
+	mux.HandleFunc("/compare", Compare)
+	mux.HandleFunc("/comparepage", Comparepage)
+	mux.HandleFunc("/manufacturer", ManufactPage)
+
+	//setting up the server parameters
+	theServer := &http.Server{
+		Addr:           address,
+		Handler:        mux,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+
+	// running the server async in another channel
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		if err := theServer.ListenAndServe(); err != http.ErrServerClosed {
+			log.Fatalf("ListenAndServe error: %v", err)
+		}
+	}()
+
+	<-sigCh
+	log.Println("\nReceived interrupt signal. Gracefully shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := theServer.Shutdown(ctx); err != nil {
+		log.Fatal("Server shutdown error:", err)
+	}
+
+	log.Println("Server has stopped please visit: https://www.youtube.com/watch?v=dQw4w9WgXcQ for more info")
 
 }
 func ManufactPage(w http.ResponseWriter, r *http.Request) {
-	temp = template.Must(template.ParseFiles("manufacturer.html"))
+	temp = template.Must(template.ParseFiles("frontend/manufacturer.html"))
 
 	manId := r.FormValue("manufacturerId")
-	manufacturer := searchbars.FilterManufacturer(manId) //add error check
-	fmt.Println(manufacturer)
+	manufacturer := searchbars.FilterManufacturer(manId)
+
 	temp.Execute(w, manufacturer)
 }
 func Comparepage(w http.ResponseWriter, r *http.Request) {
-	temp = template.Must(template.ParseFiles("comparepage.html"))
+	temp = template.Must(template.ParseFiles("frontend/comparepage.html"))
 
 	temp.Execute(w, CompList)
 }
 
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
 	// Only parse the template once
-	temp = template.Must(template.ParseFiles("index.html"))
+	temp = template.Must(template.ParseFiles("frontend/index.html"))
 	searchbar := searchbars.FindSearch()
-
+	// we create interface to post two different structs at the same go
 	manu := map[string]interface{}{
 		"search": searchbar,
 		"car":    Car2,
@@ -69,7 +106,7 @@ func Action(w http.ResponseWriter, r *http.Request) { // here we both recieve da
 	var hpInt int
 	var err error
 
-	// FormVale gives us a "string" so we need to convert it into a int
+	// FormValue gives us a "string" so we need to convert it into a int
 	yearStr := r.FormValue("cyear")
 	if yearStr == "" {
 		cyearInt = 0
@@ -140,7 +177,7 @@ func Compare(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
-	// we load te values we recieved from the sidenavbar
+	// we load te values we recieved from the compare buttons
 	comp := structs.Models{
 		Name:  r.FormValue("carName"),
 		Year:  cyearInt,
@@ -153,9 +190,6 @@ func Compare(w http.ResponseWriter, r *http.Request) {
 		},
 	}
 	CompList = append(CompList, comp)
-
-	fmt.Println(comp)
-	fmt.Println(CompList)
 
 	searchbar := searchbars.FindSearch()
 
